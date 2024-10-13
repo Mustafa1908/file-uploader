4b78db47fcb773778d7cea282427c290f46b0b6f
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-10-13 19:55:40 +0200

    Add form_upload, library routes, import multer, uplaod

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const indexController = require("../controllers/indexController.js");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 const router = express.Router();
 
 router.get("/", indexController.getHomePageGet);
+router.get("/form_upload", indexController.getFormUploadPageGet);
+router.post(
+  "/form_upload",
+  upload.single("file"),
+  indexController.formUploadPost
+);
+router.get("/library", indexController.getLibraryPage);
+router.post("/library", indexController.createFolder);
 
 module.exports = router;
