8dc6d907f9290899622991d79df46442ecfec4cc
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-10-16 16:14:23 +0200

    Add File table, add file row in Folder table

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
@@ -10,7 +11,22 @@ let getFormUploadPageGet = async (req, res) => {
 };
 
 let formUploadPost = async (req, res) => {
-  res.redirect("/");
+  bcrypt.hash(req.file.originalname, 10, async (err, hashedFileName) => {
+    try {
+      const folder = await prisma.file.create({
+        data: {
+          fileName: req.file.originalname,
+          hashedFile: hashedFileName,
+          fileSize: req.file.size,
+          folderId: req.session.folderId,
+        },
+      });
+
+      res.redirect("/folder_page");
+    } catch (error) {
+      console.error(error);
+    }
+  });
 };
 
 let getLibraryPage = async (req, res) => {
@@ -28,16 +44,41 @@ let getLibraryPage = async (req, res) => {
   });
 };
 
+let getFolderPage = async (req, res) => {
+  const folder = await prisma.folder.findUnique({
+    where: {
+      folderName: req.params.folderName,
+    },
+  });
+
+  const folderFiles = await prisma.file.findMany({
+    where: {
+      folderId: req.session.folderId,
+    },
+  });
+
+  req.session.folderId = folder.id;
+  res.render("folder_page", {
+    urlParams: req.params,
+    folderFiles: folderFiles,
+  });
+};
+
+let getFilePage = async (req, res) => {
+  const fileInformations = await prisma.file.findUnique({
+    where: {
+      fileName: req.params.fileName,
+    },
+  });
+
+  res.render("file_page", { fileData: fileInformations });
+};
+
 let createFolder = async (req, res) => {
   try {
     const folder = await prisma.folder.create({
       data: {
         folderName: req.body.folderName,
-        userId: req.session.userId,
-      },
-    });
-    const userFolders = await prisma.folder.findMany({
-      where: {
         userId: req.session.userId,
       },
     });
@@ -93,6 +134,8 @@ module.exports = {
   getFormUploadPageGet,
   formUploadPost,
   getLibraryPage,
+  getFolderPage,
+  getFilePage,
   createFolder,
   getUpdateFolderNamePage,
   updateFolderName,
