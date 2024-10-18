642256045a7b8fd880ec8812f63aa7cd58c60d6d
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-10-18 19:43:38 +0200

    Add axios, cloudinary, upload files to cloudinary, add download controller

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,13 @@
 const bcrypt = require("bcryptjs");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const axios = require("axios");
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: "dj0kpovxa", // Replace with your cloud name
+  api_key: "366613127446414", // Replace with your API key
+  api_secret: "RnaEkjR__f-8BWQl3RgyDGho4k0", // Replace with your API secret
+});
 
 let getHomePageGet = async (req, res) => {
   res.render("index", { user: req.session.userName });
@@ -13,18 +20,26 @@ let getFormUploadPageGet = async (req, res) => {
 let formUploadPost = async (req, res) => {
   bcrypt.hash(req.file.originalname, 10, async (err, hashedFileName) => {
     try {
+      // Upload file to Cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        resource_type: "auto",
+      });
+      console.log(result.secure_url);
+      // Add file to database
       const folder = await prisma.file.create({
         data: {
           fileName: req.file.originalname,
           hashedFile: hashedFileName,
           fileSize: req.file.size,
           folderId: req.session.folderId,
+          fileUrl: result.secure_url,
         },
       });
 
       res.redirect("/folder_page");
     } catch (error) {
-      console.error(error);
+      console.error("Upload failed:", error);
+      throw error;
     }
   });
 };
@@ -70,8 +85,38 @@ let getFilePage = async (req, res) => {
       fileName: req.params.fileName,
     },
   });
+  res.render("file_page", {
+    fileData: fileInformations,
+    urlParams: req.params,
+  });
+};
 
-  res.render("file_page", { fileData: fileInformations });
+let downloadFile = async (req, res) => {
+  const fileInformations = await prisma.file.findUnique({
+    where: {
+      fileName: req.params.fileName,
+    },
+  });
+
+  try {
+    // Fetch the file from the external URL
+    const fileResponse = await axios.get(fileInformations.fileUrl, {
+      responseType: "stream",
+    });
+
+    // Set the headers to force download
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename="${fileInformations.fileUrl}"`
+    );
+    res.setHeader("Content-Type", fileResponse.headers["content-type"]);
+
+    // Pipe the file data to the client
+    fileResponse.data.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error downloading file");
+  }
 };
 
 let createFolder = async (req, res) => {
@@ -136,6 +181,7 @@ module.exports = {
   getLibraryPage,
   getFolderPage,
   getFilePage,
+  downloadFile,
   createFolder,
   getUpdateFolderNamePage,
   updateFolderName,
